90518b898c94a7e5243d3d76f3eb7a5935f0a3c4
p.karari <kararikelvin2002@gmail.com>
2024-06-14 15:54:27 +0300

    City service

--- src/drizzle/city/city.router.ts
+++ src/drizzle/city/city.router.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
 import { CityController } from "./city.controller";
 
+
 const cityRouter = new Hono();
 
 cityRouter.get("/city", CityController.getAllCities);
-cityRouter.get("/city/:id", CityController.getCityById);
+cityRouter.get("/city/:id" , CityController.getCityById);
 cityRouter.post("/city", CityController.createCity);
 cityRouter.put("/city/:id", CityController.updateCity);
 cityRouter.delete("/city/:id", CityController.deleteCity);
