f87639d2e469dc3a26033f78daf0889754ff6926
p.karari <kararikelvin2002@gmail.com>
2024-06-14 17:18:47 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,8 @@ const port = 3000
 
 console.log(`Server is running on port ${port}`)
 
+
+
 app.route("/", userRouter);
 app.route("/", stateRouter);
 app.route("/", cityRouter);
@@ -39,7 +41,54 @@ app.route("/", statusRouter);
 app.route("/", driverRouter);
 app.route("/", authRouter )
 
+app.get('/', (c) => {
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Restaurant API</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f8f9fa;
+                margin: 0;
+                padding: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                color: #333;
+            }
+            .container {
+                text-align: center;
+                background: #ffffff;
+                padding: 20px;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+                color: #007bff;
+            }
+            p {
+                font-size: 1.2em;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Restaurant API</h1>
+            <p>This is a restaurant API, created by KELVIN KARARI.</p>
+        </div>
+    </body>
+    </html>
+  `;
+  return c.html(htmlContent);
+});
 serve({
   fetch: app.fetch,
   port 
 })
+
+
